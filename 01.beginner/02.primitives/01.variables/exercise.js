5a517fc5574b463323b04b2efa4e8c5e1b6e9881
sidneysmartin <sidneysmsmartin@gmail.com>
2024-11-04 22:10:54 -0300

    Learn basic variable

---
+++ 01.beginner/02.primitives/01.variables/exercise.js
@@ -0,0 +1,14 @@
+// 1. Create variable name (name) & store your name.
+// 2. Create variable name (whatDoYouWannaBecomeInYourLife) & store "programmer"
+// 3. Create variable name (gender) & store your gender.
+// 4. Create variable name (twitterHandle) & store your twitter handle.
+// 5. Finally log all variables to the console.
+
+let name = "John Smith";
+let whatDoYouWannaBecomeInYourLife = "Programmer";
+let gender = "Male";
+let twitterHandle = "@john_smith";
+console.log(name);
+console.log(whatDoYouWannaBecomeInYourLife);
+console.log(gender);
+console.log(twitterHandle);
---
+++ 01.beginner/02.primitives/01.variables/main.js
@@ -0,0 +1,14 @@
+// (reserved keyword): (variable name): (type of value)
+let name = "John Doe";
+let age = 30;
+let isMarried = false;
+
+console.log(`${name} is ${age} years old and is married: ${isMarried}`);
+
+console.log(
+  "Em JavaScript temos 3 formas de declarar variáveis: (var) (let) (const)"
+);
+console.log("Var - E a forma mais antiga tem escopo global");
+console.log(
+  "let` e `const` são limitadas ao bloco onde são definidas, enquanto `var` é visível na função inteira, SEMPRE USE (LET OU CONST)"
+);
