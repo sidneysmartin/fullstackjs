a0679622c7ba465acbdceec01403bf0e8431c271
sidneysmartin <sidneysmsmartin@gmail.com>
2024-11-08 21:07:26 -0300

    reviewing about function declarations and expression

---
+++ 01.beginner/05.functions/02.declarations-and-expression/main.js
@@ -0,0 +1,13 @@
+// Function Declaration
+function sayHello(userName) {
+  console.log(`Hello ${userName}`);
+}
+sayHello("Philip");
+
+// Function Expression
+const greetings = function (userName) {
+  console.log(`Good morning, ${userName}`);
+};
+greetings("Flávia");
+
+// -------------------------
