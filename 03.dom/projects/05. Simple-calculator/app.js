8730de65755bb380a9001d31cb5e646f4543a124
sidneysmartin <sidneysmsmartin@gmail.com>
2024-11-14 21:51:54 -0300

    build a simple calculator

---
+++ 03.dom/projects/05. Simple-calculator/app.js
@@ -0,0 +1,24 @@
+document.getElementById("btn").addEventListener("click", () => {
+  // Get Elements from the DOM
+  let num1 = document.querySelector(".num1").value;
+  let num2 = document.querySelector(".num2").value;
+  let operator = document.querySelector("#selectOp").value;
+  let result = document.querySelector(".result");
+
+  // Basic Math Operations
+
+  switch (operator) {
+    case "plus":
+      result.innerHTML = Number(num1) + Number(num2);
+      break;
+    case "min":
+      result.innerHTML = Number(num1) - Number(num2);
+      break;
+    case "dev":
+      result.innerHTML = Number(num1) / Number(num2);
+      break;
+    case "multi":
+      result.innerHTML = Number(num1) * Number(num2);
+      break;
+  }
+});
