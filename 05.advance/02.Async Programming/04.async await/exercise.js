fafab3b1adc6f108948966bd91d891b4e3ad5f67
sidneysmartin <sidneysmsmartin@gmail.com>
2024-11-18 21:36:51 -0300

    reviewing about async await

---
+++ 05.advance/02.Async Programming/04.async await/exercise.js
@@ -0,0 +1,74 @@
+"use strict";
+// Async is a special function that is designed to operate asynchronousyly, meaning that it can perform tasks in the background while other code
+// continues to execute. Async functions are marked the async keyword
+
+// --------------------------------------------------
+// Refactor this code to use async/await
+// console.log("Start");
+
+// function getUserDataFromDB(name) {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Valid Name");
+//       resolve(name);
+//     }, 2000);
+//   });
+// }
+
+// function getUserHobbies() {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       console.log("Getting user hobbies...");
+//       resolve(["Football", "Singing", "Solving Math"]);
+//     }, 2000);
+//   });
+// }
+
+// getUserDataFromDB("HuXn")
+//   .then((name) => getUserHobbies(name))
+//   .then((hobby) => console.log(hobby))
+//   .catch((err) => console.log(err));
+
+// console.log("End");
+
+// --------------------------------------------------
+
+// Refactoring
+console.log("Start");
+
+function getUserDataFromDB(name) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Valid Name");
+      resolve(name);
+    }, 2000);
+  });
+}
+
+function getUserHobbies() {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      console.log("Getting user hobbies...");
+      resolve(["Football", "Singing", "Solving Math"]);
+    }, 2000);
+  });
+}
+
+// getUserDataFromDB("HuXn")
+//   .then((name) => getUserHobbies(name))
+//   .then((hobby) => console.log(hobby))
+//   .catch((err) => console.log(err));
+
+async function showUserHobbies() {
+  try {
+    const name = await getUserDataFromDB("HuXn");
+    const hobby = await getUserHobbies(name);
+    console.log(hobby);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+showUserHobbies();
+
+console.log("End");
